d61f7787bd3b88913a604603c217fd638a2e6751
ksalama <khalid.m.salama@gmail.com>
2020-10-30 02:07:35 +0000

    Use AI Platform Prediction Custom Containers for serving ScaNN

--- retail/recommendation-system/bqml-scann/index_server/main.py
+++ retail/recommendation-system/bqml-scann/index_server/main.py
@@ -22,47 +22,46 @@ from matching import ScaNNMatcher
 
 PROJECT_ID = os.environ['PROJECT_ID']
 REGION = os.environ['REGION']
-MODEL_NAME = os.environ['MODEL_NAME']
-MODEL_VERSION = os.environ['MODEL_VERSION']
+EMBEDDNIG_LOOKUP_MODEL_NAME = os.environ['EMBEDDNIG_LOOKUP_MODEL_NAME']
+EMBEDDNIG_LOOKUP_MODEL_VERSION = os.environ['EMBEDDNIG_LOOKUP_MODEL_VERSION']
 INDEX_DIR = os.environ['INDEX_DIR']
+PORT = os.environ['PORT']
+
 
 scann_matcher = ScaNNMatcher(INDEX_DIR)
 embedding_lookup = EmbeddingLookup(
-    PROJECT_ID, REGION, MODEL_NAME, MODEL_VERSION)
+    PROJECT_ID, REGION, EMBEDDNIG_LOOKUP_MODEL_NAME, EMBEDDNIG_LOOKUP_MODEL_VERSION)
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def display_default():
-  return 'Welcome to the item similarity matching app!\n' \
-         'use /search?query=<item_Id> to start finding to similar items'
+@app.route("/v1/models/<model>/versions/<version>", methods=["GET"])
+def health(model, version):
+  return jsonify({})
 
 
-@app.route('/readiness_check')
-def check_readiness():
-  return 'App is ready!'
-
-
-@app.route('/search', methods=['GET'])
-def search():
+@app.route("/v1/models/<model>/versions/<version>:predict", methods=["POST"])
+def predict(model, version):
+  result = 'predictions'
   try:
-    query = request.args.get('query')
-    show = request.args.get('show')
-    show = '10' if show is None else show
+    data = request.get_json()['instances'][0]
+    query = data.get('query', None)
+    show = data.get('show', 10)
+    if not str(show).isdigit(): show = 10
 
     is_valid, error = validate_request(query, show)
 
-    if not is_valid:
-      results = error
+    if not is_valid: 
+      value = error
     else:
       vector = embedding_lookup.lookup([query])[0]
-      results = scann_matcher.match(vector, int(show))
+      value = scann_matcher.match(vector, int(show))
 
   except Exception as error:
-    results = 'Unexpected error: {}'.format(error)
+    value = 'Unexpected error: {}'.format(error)
+    result = 'error'
 
-  response = jsonify(results)
+  response = jsonify({result: value})
   return response
 
 
@@ -73,12 +72,9 @@ def validate_request(query, show):
   if not query:
     is_valid = False
     error = 'You need to provide the item Id(s) in the query!'
-  elif not show or not show.isdigit():
-    is_valid = False
-    error = 'Invalid show results value!'
 
   return is_valid, error
 
 
 if __name__ == '__main__':
-  app.run(host='127.0.0.1', port=8080, debug=True)
+  app.run(host='0.0.0.0', port=PORT)
