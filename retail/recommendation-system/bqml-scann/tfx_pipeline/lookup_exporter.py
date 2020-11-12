5a1668461dfce8e2db39e9af7d4ab4cb380c9ed9
ksalama <khalidsalama@google.com>
2020-11-12 16:11:17 +0000

    Update TFX pipeline to use Schema

--- retail/recommendation-system/bqml-scann/tfx_pipeline/lookup_exporter.py
+++ retail/recommendation-system/bqml-scann/tfx_pipeline/lookup_exporter.py
@@ -14,28 +14,42 @@
 
 
 import tensorflow as tf
+import tensorflow_data_validation as tfdv
+from tensorflow_transform.tf_metadata import schema_utils
 import numpy as np
 import logging
 
 VOCABULARY_FILE_NAME = 'vocabulary.txt'
 class EmbeddingLookup(tf.keras.Model):
 
-  def __init__(self, embedding_files_prefix, **kwargs):
+  def __init__(self, embedding_files_prefix, schema_file_path, **kwargs):
     super(EmbeddingLookup, self).__init__(**kwargs)
-
+    
     vocabulary = list()
     embeddings = list()
 
+    logging.info('Loading schema...')
+    schema = tfdv.load_schema_text(schema_file_path)
+    feature_sepc = schema_utils.schema_as_feature_spec(schema).feature_spec
+    logging.info('Schema is loadded.')
+    
+    def _gzip_reader_fn(filenames):
+      return tf.data.TFRecordDataset(filenames, compression_type='GZIP')
+    
+    dataset = tf.data.experimental.make_batched_features_dataset(
+      embedding_files_prefix, 
+      batch_size=1, 
+      num_epochs=1,
+      features=feature_sepc,
+      reader=_gzip_reader_fn,
+      shuffle=False
+    )
+
     # Read embeddings from tfrecord files.
     logging.info('Loading embeddings from files ...')
-    embedding_files = tf.io.gfile.glob(embedding_files_prefix)
-    dataset = tf.data.TFRecordDataset(embedding_files, compression_type="GZIP")
-    for tfrecord in dataset:
-      example = tf.train.Example.FromString(tfrecord.numpy())
-      item_Id = example.features.feature['item_Id'].bytes_list.value[0].decode()
-      embedding = np.array(example.features.feature['embedding'].float_list.value)
-      vocabulary.append(item_Id)
-      embeddings.append(embedding)
+    for tfrecord_batch in dataset:
+      vocabulary.append(tfrecord_batch["item_Id"].numpy()[0][0].decode())
+      embeddings.append(tfrecord_batch["embedding"].numpy()[0])
     logging.info('Embeddings loaded.')
     
     embedding_size = len(embeddings[0])
@@ -75,10 +89,11 @@ def run_fn(params):
 
   embedding_files_path = params.train_files
   model_output_dir = params.serving_model_dir
+  schema_file_path = params.schema_file
 
   logging.info('Instantiating embedding lookup model...')
-  embedding_lookup_model = EmbeddingLookup(embedding_files_path)
-  logging.info('Model is Instantiated.')
+  embedding_lookup_model = EmbeddingLookup(embedding_files_path, schema_file_path)
+  logging.info('Model is instantiated.')
   
   signatures = {
     'serving_default': embedding_lookup_model.__call__.get_concrete_function(),
